389f4df19e8865e91bdede8f877d6c377aa95930
Funky Panda <72228432+Funky-Panda@users.noreply.github.com>
2025-08-11 01:49:56 +0100

    changed requirments

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sultanrasul/Desktop/projects/Iain Marr Antiques/venv"
+set -gx VIRTUAL_ENV "/Users/sultanrasul/Desktop/projects/Iain Marr Antiques/Backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
